e879a5ef6a726c6d20e72c2348bad48e7a2a1b60
jiji sreejesh <jijibsc@gmail.com>
2024-06-03 12:14:58 +0530

    second commit

--- hello.c
+++ hello.c
@@ -2,8 +2,12 @@
 // #include<conio.h>
 int main(){
     int a = 5;
-   
+    float b=43.5;
+    char s='d';
     printf("%d",a);
+    printf("\n%f",b);
+    printf("\n%c",s);
+
    
    return 0;
 }
