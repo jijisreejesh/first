46f2747804fe417a6e4e1545c08797ac148d5ef2
jiji sreejesh <jijibsc@gmail.com>
2024-06-04 18:46:50 +0530

    second commit

--- first1.c
+++ first1.c
@@ -2,8 +2,10 @@
 int main(){
     int a,b;
     printf("Enter 2 numbers");
-    fflush(stdout);
+    //fflush(stdout);
     scanf("%d%d",&a,&b);
     printf("Numbers are :%d and  %d ",a,b);
+     printf("\nsum is: %d ",a+b);
+    
     return 0;
 }
